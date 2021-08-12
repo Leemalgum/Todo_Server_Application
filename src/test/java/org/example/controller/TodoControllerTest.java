52e727e11ea8568d2f275bdf4e374f031e8a57b8
leemalgum <60692335+Leemalgum@users.noreply.github.com>
2021-08-13 00:26:20 +0900

    TodoServiceTest

---
+++ src/test/java/org/example/controller/TodoControllerTest.java
@@ -0,0 +1,41 @@
+package org.example.controller;
+
+import org.example.model.TodoEntity;
+import org.example.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TodoController.class)
+class TodoControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    TodoService todoService;
+
+    private TodoEntity expected;
+
+    @BeforeEach
+    void setup() {
+        this.expected = new TodoEntity();
+        this.expected.setId(123L);
+        this.expected.setTitle("TEST TITLE");
+        this.expected.setOrder(0L);
+        this.expected.setCompleted(false);
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void readOne() {
+    }
+}
